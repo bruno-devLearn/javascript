09b1b4b6412a8796e36b26accde1fe2c7416a9a9
bruno-devLearn <qbruno570@gmail.com>
2025-02-16 12:56:01 -0300

    correções e sistema de pontuação

--- exercicios/ex012/ex12.js
+++ exercicios/ex012/ex12.js
@@ -5,7 +5,7 @@ titulo.textContent = "Quiz";
 let questao = document.getElementById("questao");
 let nQuestao = document.getElementById("nQuestao");
 let pergunta = document.getElementById("pergunta");
-let qtdQuestoes = 0;
+let qtdQuestoes;
 
 let a = document.getElementById("a");
 let b = document.getElementById("b");
@@ -14,6 +14,8 @@ let d = document.getElementById("d");
 
 let numero = document.getElementById("numero");
 const total = document.getElementById("total");
+
+let resposta;
 
 // coloca o data.json numa variavel
 const url = "data.json";
@@ -39,47 +41,71 @@ let index = 1;
 
 // exibe as questoes na tela
 function exibir(data) {
-    nQuestao.textContent = data.questoes[index].numQuestao;
-    pergunta.textContent = data.questoes[index].pergunta;
+    if (index < qtdQuestoes) {
+        nQuestao.textContent = data.questoes[index].numQuestao;
+        pergunta.textContent = data.questoes[index].pergunta;
 
-    a.textContent = data.questoes[index].alternativaA;
-    b.textContent = data.questoes[index].alternativaB;
-    c.textContent = data.questoes[index].alternativaC;
-    d.textContent = data.questoes[index].alternativaD;
+        a.textContent = data.questoes[index].alternativaA;
+        b.textContent = data.questoes[index].alternativaB;
+        c.textContent = data.questoes[index].alternativaC;
+        d.textContent = data.questoes[index].alternativaD;
 
-    numero.textContent = data.questoes[index].numQuestao;
-    total.textContent = 12;
+        numero.textContent = data.questoes[index].numQuestao;
+        total.textContent = qtdQuestoes - 1;
+    }
+    verificaResposta(data);
 }
 
 // passa para a proxima questao
 function proximaQuestao() {
     if (index < qtdQuestoes) {
         index++;
-        if (index == 13) {
+        if (index == qtdQuestoes) {
             resultado();
         }
     }
-    pegarDados();
+}
+
+// variaveis de sons
+const somAcerto = document.getElementById("somAcerto");
+const somErro = document.getElementById("somErro");
+
+let pontos = 0;
+
+function verificaResposta(data) {
+    if (resposta == data.questoes[index - 1].correta) {
+        pontos += 10;
+    }
+    console.log(pontos);
+    return pontos;
 }
 
 // adiciona eventos de cliques
 a.addEventListener("click", function () {
     if (index < qtdQuestoes) {
+        resposta = "a";
+        pegarDados();
         proximaQuestao();
     }
 });
 b.addEventListener("click", function () {
     if (index < qtdQuestoes) {
+        resposta = "b";
+        pegarDados();
         proximaQuestao();
     }
 });
 c.addEventListener("click", function () {
     if (index < qtdQuestoes) {
+        resposta = "c";
+        pegarDados();
         proximaQuestao();
     }
 });
 d.addEventListener("click", function () {
     if (index < qtdQuestoes) {
+        resposta = "d";
+        pegarDados();
         proximaQuestao();
     }
 });
