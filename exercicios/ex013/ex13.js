45ff9d856cf22f0c2391111134612be09f5d8616
bruno-devLearn <qbruno570@gmail.com>
2025-02-17 09:18:07 -0300

    melhor funçao

--- exercicios/ex013/ex13.js
+++ exercicios/ex013/ex13.js
@@ -5,59 +5,40 @@ let hMainText = document.getElementById("h");
 let minMainText = document.getElementById("m");
 let secMainText = document.getElementById("s");
 
-function pegarDataMain() {
-    horaAtual = new Date();
-
-    let hMain = horaAtual.getHours();
-    let minMain = horaAtual.getMinutes();
-    let secMain = horaAtual.getSeconds();
-
-    let hMainStr = String(hMain);
-    let minMainStr = String(minMain);
-    let secMainStr = String(secMain);
-
-    hMainStr.length == 1
-        ? (hMainText.textContent = "0" + hMain)
-        : (hMainText.textContent = hMainStr);
-
-    minMainStr.length == 1
-        ? (minMainText.textContent = "0" + minMain)
-        : (minMainText.textContent = minMainStr);
-
-    secMainStr.length == 1
-        ? (secMainText.textContent = "0" + secMain)
-        : (secMainText.textContent = secMainStr);
-}
-
 let hSmartText = document.getElementById("hSmart");
 let minSmartText = document.getElementById("mSmart");
 let secSmartText = document.getElementById("sSmart");
 
-function pegarDataSmart() {
+function pegarHora() {
     horaAtual = new Date();
 
-    let hSmart = horaAtual.getHours();
-    let minSmart = horaAtual.getMinutes();
-    let secSmart = horaAtual.getSeconds();
+    let h = horaAtual.getHours();
+    let min = horaAtual.getMinutes();
+    let sec = horaAtual.getSeconds();
 
-    let hSmartStr = String(hSmart);
-    let minSmartStr = String(minSmart);
-    let secSmartStr = String(secSmart);
+    let hStr = String(h);
+    let minStr = String(min);
+    let secStr = String(sec);
 
-    hSmartStr.length == 1
-        ? (hSmartText.textContent = "0" + hSmart)
-        : (hSmartText.textContent = hSmartStr);
+    if (hStr.length == 1) {
+        hStr = "0" + h;
+    }
+    if (minStr.length == 1) {
+        minStr = "0" + min;
+    }
+    if (secStr.length == 1) {
+        secStr = "0" + sec;
+    }
 
-    minSmartStr.length == 1
-        ? (minSmartText.textContent = "0" + minSmart)
-        : (minSmartText.textContent = minSmartStr);
+    hMainText.textContent = hStr;
+    minMainText.textContent = minStr;
+    secMainText.textContent = secStr;
 
-    secSmartStr.length == 1
-        ? (secSmartText.textContent = "0" + secSmart)
-        : (secSmartText.textContent = secSmartStr);
+    hSmartText.textContent = hStr;
+    minSmartText.textContent = minStr;
+    secSmartText.textContent = secStr;
 }
 
 setInterval(() => {
-    pegarDataMain();
-    pegarDataSmart();
+    pegarHora();
 }, 1000);
