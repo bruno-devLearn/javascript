5c81ce7f71ae74c9cef0df1ff9a4791eb96fcb3a
bruno-devLearn <qbruno570@gmail.com>
2025-03-18 12:29:43 -0300

    aula 004

---
+++ aula004/aula04.js
@@ -0,0 +1,19 @@
+/*
++ soma
+- subtração
+* multiplicação
+/ divisao
+% resto
+++ incremento
+-- decremento
+*/
+
+let num1 = 0;
+let num2 = 0;
+let res = 0;
+
+num1 = 15;
+num2 = 2;
+
+num1 /= 5;
+console.log(num1);
