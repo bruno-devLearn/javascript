73ec37cdc61eacfb811d33f393c137d1ee0873b9
bruno-devLearn <qbruno570@gmail.com>
2025-03-20 08:10:58 -0300

    aula 010

---
+++ aula010/aula10.js
@@ -0,0 +1,12 @@
+let num = 10;
+
+let res = num % 2;
+
+if (res == 0) {
+    console.log("Par");
+} else {
+    console.log("Impar");
+}
+
+let res2 = num % 2 == 0 ? "Par" : "Impar";
+console.log(res2);
