e8bd17c291d8a95b5253b51bd862fdf8e0062f90
bruno-devLearn <qbruno570@gmail.com>
2025-03-20 07:43:09 -0300

    aula 009

---
+++ aula009/aula09.js
@@ -0,0 +1,23 @@
+let n = 10;
+
+// pos-incremento
+console.log("n++: " + n++ + " / " + n);
+
+// pre-decremento
+console.log("++n: " + n-- + " / " + n);
+
+// pos-incremento
+console.log("n--: " + ++n + " / " + n);
+
+// pre-decremento
+console.log("--n: " + --n + " / " + n);
+
+// atribuição composta positiva
+n += 10;
+
+console.log("n += 10: " + n + " / " + n);
+
+// atribuição composta negativa
+n -= 10;
+
+console.log("n -= 10: " + n + " / " + n);
