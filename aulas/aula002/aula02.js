5e1eb2fad1ccb5f3b9946f57dec1323cd641f695
bruno-devLearn <qbruno570@gmail.com>
2025-05-14 09:18:44 -0300

    aula 002

---
+++ aulas/aula002/aula02.js
@@ -0,0 +1,30 @@
+/* Arquivo JS externo */
+
+document.write("Olá, Mundo!");
+console.log("Outro Olá, Mundo!");
+// alert("Olá, Mundo com alerta");
+
+let mensagem = "Minha mensagem";
+let mensagem2 = "Outra mensagem";
+let meuPeso = 45.5;
+let minhaAltura = 1.49;
+let minhaIdade = 16;
+let ehDoador = false;
+let teste = null;
+
+// criar constantes e atribuir valor
+const PI = 3.1415;
+const TAXA = 0.5;
+
+teste = "Algum conteudo";
+
+// escrever conteudo de variaveis e constantes
+document.write("<p> Mensagem: " + mensagem + "<p>");
+document.write(` Mensagem2: ${mensagem2}`);
+document.write("<p> Peso: " + meuPeso + "<p>");
+document.write(` Altura: ${minhaAltura}`);
+document.write("<p> Idade: " + minhaIdade + "<p>");
+document.write(` Doador? ${ehDoador}`);
+document.write("<p> Teste: " + teste + "<p>");
+document.write("<p> Pi: " + PI + "<p>");
+document.write("<p> Taxa: " + TAXA + "<p>");
