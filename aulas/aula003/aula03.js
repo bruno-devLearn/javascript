cadcdb0d2f96db3d4452865a6abb25dad6bbe102
bruno-devLearn <qbruno570@gmail.com>
2025-05-14 11:48:38 -0300

    aula 003

---
+++ aulas/aula003/aula03.js
@@ -0,0 +1,33 @@
+/* Arquivo JS externo */
+
+let n1 = 10; // number inteiro
+let n2 = 2; // number inteiro
+let n3 = "2"; // string texto
+
+document.write(`N1: ${n1} <br>`);
+document.write(`N2: ${n2} <br>`);
+document.write(`N3: ${n3} <br>`);
+
+console.log(n1 + n2); // adição
+console.log(n1 - n2); // subtração
+console.log(n1 * n2); // multiplicação
+console.log(n1 / n2); // divisão
+console.log(n1 % n2); // resto
+
+console.log(n1 == n3); // igual
+console.log(n1 === n3); // identico
+console.log(n1 != n3); // nao igual
+console.log(n1 !== n3); // nao identico
+
+console.log(n1 > n2); // maior
+console.log(n1 < n2); // menor
+console.log(n1 >= n2); // maior ou igual
+console.log(n1 >= n2); // menor ou igual
+
+let a = true;
+let b = false;
+
+console.log(a && b); // E
+console.log(a || b); // OU
+console.log(!a); // nao
+console.log(!b); // nao
