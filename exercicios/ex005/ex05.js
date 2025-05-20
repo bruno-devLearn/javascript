a42affbfcead2bb78bbed59a738a78cb4d722da0
bruno-devbr <qbruno570@gmail.com>
2025-05-20 08:22:40 -0300

    ex 005

--- exercicios/ex005/ex05.js
+++ exercicios/ex005/ex05.js
@@ -6,15 +6,32 @@ function gerarNumero() {
     let max = 10;
     let min = 1;
     randomNumber = Math.floor(Math.random() * max) + min;
+
+    console.log(randomNumber);
     return randomNumber;
 }
 
 // gera o numero assim que a pagina carregar
 window.addEventListener("load", gerarNumero);
 
-// DOM do input, menssagem de aviso e botao
-const input = document.querySelector("#inputNumero");
-const menssage = document.querySelector("#aviso");
+// musica de fundo
+const sound = document.querySelector("#musicaDeFundo");
+
+// logica do btn de musica
+function musicBg() {
+    if (sound.paused) {
+        sound.play();
+    } else {
+        sound.pause();
+        sound.currentTime = 0;
+    }
+}
+
+// bota e evento de clique adicionando a ele
+const soundBtn = document.querySelector("#btnSom");
+soundBtn.addEventListener("click", musicBg);
+
+// DOM do botao de jogo
 const sendBtn = document.querySelector("#btnChutar");
 
 // bloqueia o btn
@@ -31,23 +48,87 @@ function ativarBtn() {
     sendBtn.style.cursor = "pointer";
 }
 
+// DOM do input e da menssagem
+const input = document.querySelector("#inputNumero");
+const menssageText = document.querySelector("#aviso");
+
+function flashMenssage(menssage) {
+    bloquearBtn();
+    menssageText.textContent = menssage;
+
+    setTimeout(() => {
+        menssageText.textContent = "";
+        menssageText.classList.remove("errou");
+        menssageText.classList.remove("acertou");
+        input.value = "";
+    }, 3000);
+}
+
 // caso o numero seja invalido da uma flash menssage e bloquei o botao
 function numeroValido() {
-    let inputValue = parseInt(input.value);
+    const inputValue = parseInt(input.value);
 
     if (inputValue < 1 || inputValue > 10 || isNaN(inputValue)) {
-        menssage.textContent =
-            "Você não está sendo um(a) mentalista! Digite um número inteiro entre 1 e 10.";
-        menssage.classList.add("errou");
-        bloquearBtn();
-
-        setTimeout(() => {
-            menssage.textContent = "";
-            menssage.classList.remove("errou");
-            ativarBtn();
-        }, 3000);
+        flashMenssage(
+            "Você não está sendo um(a) mentalista! Digite um número inteiro entre 1 e 10."
+        );
+        menssageText.classList.add("errou");
     }
 }
 
-// assim que perder o foco do input executa a função
+// assim que perder o foco do input executa a função; assim que clicar no input toca a musica
 input.addEventListener("blur", numeroValido);
+input.addEventListener("click", () => {
+    sound.play();
+    ativarBtn();
+});
+
+let chute = "";
+
+function jogo() {
+    const inputValue = input.value;
+
+    if (inputValue < randomNumber) {
+        chute = "menor";
+    } else if (inputValue > randomNumber) {
+        chute = "maior";
+    } else {
+        chute = "certo";
+    }
+
+    return chute;
+}
+
+function jogoSwitch() {
+    jogo();
+
+    switch (chute) {
+        case "menor":
+            flashMenssage("Chute menor que o número secreto");
+            menssageText.classList.add("errou");
+            break;
+        case "maior":
+            flashMenssage("Chute maior que o número secreto");
+            menssageText.classList.add("errou");
+            break;
+        case "certo":
+            flashMenssage("Acertou, o número secreto era " + randomNumber);
+            menssageText.classList.add("acertou");
+            gerarNumero();
+            break;
+    }
+}
+
+const playBtn = document.querySelector("#btnChutar");
+
+playBtn.addEventListener("click", (e) => {
+    const inputValue = input.value;
+
+    if (inputValue < 1 || inputValue > 10 || isNaN(inputValue)) {
+        numeroValido();
+    } else {
+        jogoSwitch();
+    }
+
+    e.preventDefault();
+});
