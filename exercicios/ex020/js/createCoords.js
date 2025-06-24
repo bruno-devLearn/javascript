4297a1fa3b7e771dfe8e0f7e9cbc75d788897baa
bruno-devbr <brunoqueiroz0707@gmail.com>
2025-06-24 12:07:00 -0300

    criação das linhas e pontos de coordenadas

---
+++ exercicios/ex020/js/createCoords.js
@@ -0,0 +1,65 @@
+let topPositions = [
+    -2, 25, 72, 108, 140, 170, 195, 223, 250, 276, 302, 330, 358, 384, 413, 445,
+    481, 528, 553,
+];
+
+let leftPositions = [
+    0, 48, 96, 143, 191, 238, 286, 334, 381, 430, 477, 525, 572, 620, 668, 715,
+    763, 811, 859,
+];
+
+const mapa = document.querySelector(".coords");
+
+function createCoords() {
+    let latIndex = 90;
+    let latString = "N";
+
+    let longIndex = 180;
+    let longString = "W";
+
+    for (let y = 0; y < topPositions.length; y++) {
+        const latLinha = document.createElement("div");
+        latLinha.style.width = "100%";
+        latLinha.style.height = "4px";
+        latLinha.style.position = "absolute";
+        latLinha.style.top = topPositions[y] + "px";
+
+        for (let x = 0; x < leftPositions.length; x++) {
+            const longPoint = document.createElement("div");
+            longPoint.style.position = "absolute";
+            longPoint.style.left = leftPositions[x] + "px";
+
+            // Definir longitude
+            if (x > 0 && x < 9) {
+                longIndex -= 20;
+                longString = "W";
+            } else if (x === 9) {
+                longIndex = 0;
+                longString = "";
+            } else if (x > 9) {
+                longIndex += 20;
+                longString = "E";
+            }
+
+            longPoint.setAttribute("data-long", longIndex + longString);
+            latLinha.appendChild(longPoint);
+        }
+
+        mapa.appendChild(latLinha);
+
+        if (y > 0 && y < 9) {
+            latIndex -= 10;
+            latString = "N";
+        } else if (y === 9) {
+            latIndex = 0;
+            latString = "";
+        } else if (y > 9) {
+            latIndex += 10;
+            latString = "S";
+        }
+
+        latLinha.setAttribute("data-lat", latIndex + latString);
+    }
+}
+
+createCoords();
