544d05adc316e397581af7a43a7f5ddcab6c3243
bruno-devbr <brunoqueiroz0707@gmail.com>
2025-07-16 22:14:49 -0300

    criação da classe Pessoa | ex 021

---
+++ exercicios/ex021/js/models/Pessoa.js
@@ -0,0 +1,90 @@
+// Classe que representa uma pessoa e calcula seu IMC e classificação
+export class Pessoa {
+    // Propriedades privadas
+    #nome;
+    #idade;
+    #peso;
+    #altura;
+    #imc;
+    #classificacao;
+
+    // Construtor inicializa os atributos e calcula IMC e classificação
+    constructor(nome, idade, peso, altura) {
+        this.#nome = nome;
+        this.#idade = idade;
+        this.#peso = peso;
+        this.#altura = altura;
+        this.#imc = this.calculaIMC(this.#peso, this.#altura);
+        this.#classificacao = this.classificaIMC(this.#imc);
+    }
+
+    // Método para calcular o IMC
+    calculaIMC(peso, altura) {
+        return peso / (altura * altura);
+    }
+
+    // Método para classificar o IMC conforme faixas
+    classificaIMC(imc) {
+        if (imc >= 40) {
+            return "Obesidade III";
+        } else if (imc >= 35) {
+            return "Obesidade II";
+        } else if (imc >= 30) {
+            return "Obesidade I";
+        } else if (imc >= 25) {
+            return "Sobrepeso";
+        } else if (imc >= 18.5) {
+            return "Peso normal";
+        } else {
+            return "Abaixo do peso";
+        }
+    }
+
+    // Getters para acessar os atributos privados
+    get nome() {
+        return this.#nome;
+    }
+
+    get idade() {
+        return this.#idade;
+    }
+
+    get peso() {
+        return this.#peso;
+    }
+
+    get altura() {
+        return this.#altura;
+    }
+
+    get imc() {
+        return this.#imc;
+    }
+
+    get classificacao() {
+        return this.#classificacao;
+    }
+
+    // Setters para modificar os atributos privados
+    set nome(novoNome) {
+        this.#nome = novoNome;
+    }
+
+    set idade(novaIdade) {
+        this.#idade = novaIdade;
+    }
+
+    // Ao alterar peso, recalcula IMC e classificação
+    set peso(novoPeso) {
+        this.#peso = novoPeso;
+        this.#imc = this.calculaIMC(this.#peso, this.#altura);
+        this.#classificacao = this.classificaIMC(this.#imc);
+    }
+
+    // Ao alterar altura, recalcula IMC e classificação
+    set altura(novaAltura) {
+        this.#altura = novaAltura;
+        this.#imc = this.calculaIMC(this.#peso, this.#altura);
+        this.#classificacao = this.classificaIMC(this.#imc);
+    }
+}
